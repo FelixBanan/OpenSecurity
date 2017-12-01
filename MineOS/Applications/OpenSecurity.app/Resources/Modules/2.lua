fc509f0bf0303534c7f89200967d92de832fe021
FelixBanan <solgier7@yandex.ru>
2017-12-01 22:15:12 +0400

    Локал переменные

--- MineOS/Applications/OpenSecurity.app/Resources/Modules/2.lua
+++ MineOS/Applications/OpenSecurity.app/Resources/Modules/2.lua
@@ -46,19 +46,19 @@ if check == "zaebis" then
   local container = window.contentContainer:addChild(GUI.container(1, 1, window.contentContainer.width, window.contentContainer.height))
   local layout = container:addChild(GUI.layout(1, 1, container.width, window.contentContainer.height, 3, 1))
   layout:setCellPosition(2, 1, layout:addChild(GUI.input(1, 1, 30, 3, 0xFFFFFF, 0x444444, 0xAAAAAA, 0xFFFFFF, 0x2D2D2D, "", "Название карточки"))).onInputFinished = function(mainContainer, input, eventData, text)
-  local login = text
+login = text
 end
 
 layout:setCellPosition(2, 1, layout:addChild(GUI.input(1, 1, 30, 3, 0xFFFFFF, 0x444444, 0xAAAAAA, 0xFFFFFF, 0x2D2D2D, "", "Пароль для карточки"))).onInputFinished = function(mainContainer, input, eventData, text)
-local password = text
+password = text
 end
 
 local comboBox = layout:setCellPosition(2, 1, layout:addChild(GUI.comboBox(1, 1, 30, 3, 0xFFFFFF, 0x444444, 0xCCCCCC, 0x888888)))
 comboBox:addItem("Заблокировать перезапись").onTouch = function()
-local lock = "true"
+lock = "true"
 end
 comboBox:addItem("Не блокировать перезапись").onTouch = function()
-local lock = "false"
+lock = "false"
 end
 
 layout:setCellPosition(2, 1, layout:addChild(GUI.roundedButton(2, 6, 30, 3, 0xBBBBBB, 0xFFFFFF, 0x999999, 0xFFFFFF, "Записать"))).onTouch = function()
